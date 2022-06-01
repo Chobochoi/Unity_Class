255ddcbfe06ce7878a2df2cb7a2e21f855e693d9
Chobochoi <102361334+Chobochoi@users.noreply.github.com>
2022-06-01 21:47:00 +0900

    Unity_Day16
    Animation
    
    Left Rigth Front Back walk

---
+++ Game Class Re/Assets/Class/Animation/Scene/Script/BlendAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendAnimation : MonoBehaviour
+{
+    Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", horizontal);
+        animator.SetFloat("Vertical", vertical);
+    }
+}
